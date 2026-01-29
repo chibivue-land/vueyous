d67aa2d802a18024dd05e0424e5324029bd323a9
naokihaba <59875779+naokihaba@users.noreply.github.com>
2026-01-30 03:05:00 +0900

    chore: remove duplicate entries from Japanese sidebar

--- packages/.vitepress/config/ja.ts
+++ packages/.vitepress/config/ja.ts
@@ -8,9 +8,6 @@ const nav: i18nTheme["nav"] = [
 const sidebar: i18nTheme["sidebar"] = [
   { text: "はじめに", link: "/ja/guide/" },
   { text: "VueUseとは", link: "/ja/guide/what-is-vueuse" },
-  { text: "VueUseの主要な要素", link: "/ja/guide/vueuse-architecture" },
-  { text: "環境構築", link: "/ja/guide/setup" },
-  { text: "貢献", link: "/ja/guide/contributing" },
 ];
 
 export const ja: i18nTheme = { nav, sidebar };

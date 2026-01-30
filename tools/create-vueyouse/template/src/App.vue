87fcc32318ac708c35dfcba4e9a69a259b5c0061
naokihaba <59875779+naokihaba@users.noreply.github.com>
2026-01-30 15:49:14 +0900

    feat: add initial template files for VueYous project

---
+++ tools/create-vueyouse/template/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  Hello VueYous!
+</template>
---
+++ tools/create-vueyouse/template/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import { HelloVueYous } from "vueyouse";
+import App from "./App.vue";
+
+HelloVueYous();
+
+createApp(App).mount("#app");
